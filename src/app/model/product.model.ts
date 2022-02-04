e723b33752e97d2a972654322d3e30a4e68001a9
Sylwester Biega <biegson11@gmail.com>
2022-02-04 22:41:37 +0100

    add productsPerPage functionality

--- src/app/model/product.model.ts
+++ src/app/model/product.model.ts
@@ -4,6 +4,6 @@ export class Product {
         public name?: string,
         public category?: string,
         public description?: string,
-        public pice?: number 
+        public price?: number 
     ) {}
 }
--- src/app/store/store.component.ts
+++ src/app/store/store.component.ts
@@ -9,17 +9,43 @@ import { ProductRepository } from '../model/product.repository';
 })
 export class StoreComponent implements OnInit {
 
+  public selectedCategory: any = null;
+  public productsPerPage = 4;
+  public selectedPage = 1;
+
   constructor(private repository: ProductRepository) { }
 
   ngOnInit(): void {
   }
 
-  get products(): Product[]{
-    return this.repository.getProducts();
-  }
+  get products(): Product[] {
+    let pageIndex = (this.selectedPage - 1) * this.productsPerPage
+    return this.repository.getProducts(this.selectedCategory)
+        .slice(pageIndex, pageIndex + this.productsPerPage);
+}
 
   get categories(): string[] {
     return this.repository.getCategories();
+  }
+
+  changeCategory(newCategory?: string){
+    this.selectedCategory = newCategory;
+  }
+
+  changePage(newPage: number){
+    this.selectedPage = newPage;
+  }
+
+  changePageSize(newSize){
+    this.productsPerPage = Number((newSize.target as HTMLInputElement).value);
+    this.changePage(1);
+    console.log(this.pageNumbers);
+  }
+
+  get pageNumbers(): number[] {
+    return Array(Math.ceil(this.repository
+      .getProducts(this.selectedCategory).length / this.productsPerPage))
+        .fill(0).map((x, i) => i+1);
   }
 
 }
