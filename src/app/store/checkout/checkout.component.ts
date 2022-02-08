dbef21a03d0aa7f443dd7fae870f6e47bd66b212
Sylwester Biega <biegson11@gmail.com>
2022-02-08 09:08:24 +0100

    add form to checkout. Making orders work

--- src/app/store/checkout/checkout.component.ts
+++ src/app/store/checkout/checkout.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Order } from 'src/app/model/order.model';
+import { OrderRepository } from 'src/app/model/order.repository';
 
 @Component({
   selector: 'app-checkout',
@@ -6,8 +9,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./checkout.component.css']
 })
 export class CheckoutComponent implements OnInit {
+  orderSent: boolean = false;
+  submitted: boolean = false;
 
-  constructor() { }
+  constructor(public repository: OrderRepository, public order: Order) { }
+
+  submitOrder(form: NgForm){
+    this.submitted = true;
+    if(form.valid) {
+      this.repository.saveOrder(this.order).subscribe(order => {
+        order.clear();
+        this.orderSent = true;
+        this.submitted = false;
+      });
+    }
+  }
 
   ngOnInit(): void {
   }
