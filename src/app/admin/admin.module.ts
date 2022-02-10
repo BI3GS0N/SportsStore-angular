70bb5d01a2277e9cdaf4ff817ca62a5383ef9082
Sylwester Biega <biegson11@gmail.com>
2022-02-10 09:35:10 +0100

    add order-table, product-editor, product-table components. add routing to these components

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -5,15 +5,27 @@ import { AuthComponent } from './auth/auth.component';
 import { AdminComponent } from './admin.component';
 import { RouterModule } from '@angular/router';
 import { AuthGuard } from './auth/auth.guard';
+import { ProductTableComponent } from './product-table/product-table.component';
+import { OrderTableComponent } from './order-table/order-table.component';
+import { ProductEditorComponent } from './product-editor/product-editor.component';
 
 let routing = RouterModule.forChild([
-  { path: "auth", component: AuthComponent},
-  { path: "main", component: AdminComponent, canActivate: [AuthGuard]},
+  { path: "auth", component: AuthComponent },
+  {
+    path: "main", component: AdminComponent, canActivate: [AuthGuard],
+    children: [
+      { path: "products/:mode/:id", component: ProductEditorComponent },
+      { path: "products/:mode", component: ProductEditorComponent },
+      { path: "products", component: ProductTableComponent },
+      { path: "orders", component: OrderTableComponent },
+      { path: "**", redirectTo: "products"}
+    ]
+  },
   { path: "**", redirectTo: "auth"}
-])
+]);
 
 @NgModule({
-  declarations: [ AuthComponent, AdminComponent],
+  declarations: [ AuthComponent, AdminComponent, ProductTableComponent, OrderTableComponent, ProductEditorComponent],
   imports: [
     CommonModule,
     FormsModule,
